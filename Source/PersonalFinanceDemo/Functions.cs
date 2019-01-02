1f015793e1db192f35a056c2ceda4ff40b1d7e71
Andrei Averin <averin.official@gmail.com>
2019-01-02 17:10:55 +0100

    Added the first version.

---
+++ Source/PersonalFinanceDemo/Functions.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace PersonalFinanceDemo
+{
+    public class Functions
+    {
+        /// <summary>
+        /// Effective Interest Rate
+        /// </summary>
+        /// <param name="rate">Nominal interest rate</param>
+        /// <param name="compPeriods">Number of compounding periods per year</param>
+        public static double EFFECT(double rate, int compPeriods)
+        {
+            return Math.Pow((1 + rate / compPeriods), compPeriods) - 1;
+        }
+
+        /// <summary>
+        /// Nominal Interest Rate
+        /// </summary>
+        /// <param name="rate">Effective interest rate</param>
+        /// <param name="compPeriods">Number of compounding periods per year</param>        
+        public static double NOMINAL(double rate, int compPeriods)
+        {
+            return compPeriods * (Math.Pow(rate + 1, 1 / (double)compPeriods) - 1);
+        }
+
+        /// <summary>
+        /// Future value of investment
+        /// </summary>
+        /// <param name="presentValue">Present value (initial capital)</param>
+        /// <param name="rate">Effective interest rate (annual)</param>
+        /// <param name="periods">Total number of periods</param>
+        /// <param name="compPeriods">Number of compounding periods per year</param>
+        /// <param name="payment">Amount of the constant periodic payment</param>
+        public static double FV(double presentValue, double rate, int periods, int compPeriods, double payment)
+        {
+            // Calculate the nominal interest rate
+            double nominal = NOMINAL(rate / 100, compPeriods);
+
+            // Re-calculate nominal rate with regard to the compounding periods
+            var compRate = nominal / compPeriods;
+
+            // Calculate first part
+            double firstPart = presentValue * Math.Pow(1 + compRate, periods);
+
+            // Get the future value
+            var result = firstPart + payment * (Math.Pow((1 + compRate), periods) - 1) / compRate;
+
+            // Format the output
+            return Math.Round(Math.Abs(result), 4);
+        }
+
+        /// <summary>
+        /// Present value of investment (initial capital)
+        /// </summary>
+        /// <param name="futureValue">Present value (initial capital)</param>
+        /// <param name="rate">Effective interest rate</param>
+        /// <param name="periods">Total number of periods</param>
+        /// <param name="compPeriods">Number of compounding periods per year</param>
+        /// <param name="payment">Amount of the constant periodic payment</param>
+        public static double PV(double futureValue, double rate, int periods, int compPeriods, double payment)
+        {
+            // Calculate the nominal interest rate
+            double nominal = NOMINAL(rate / 100, compPeriods);
+
+            // Re-calculate nominal rate with regard to the compounding periods
+            var compRate = nominal / compPeriods;
+
+            // Get the future value
+            var result = (futureValue - Math.Abs(payment) * (Math.Pow((1 + compRate), periods) - 1) / compRate) / Math.Pow(1 + compRate, periods);
+
+            // Format the output (negative)
+            return -1 * Math.Round(Math.Abs(result), 4);
+        }
+
+        /// <summary>
+        /// Constant periodic payment
+        /// </summary>
+        /// <param name="presentValue">Present value (initial capital)</param>
+        /// <param name="futureValue">Future value of the investment</param>
+        /// <param name="rate">Interest rate</param>
+        /// <param name="periods">Total number of periods</param>
+        /// <param name="compPeriods">Number of compounding periods per year</param>
+        public static double PMT(double presentValue, double futureValue, double rate, int periods, int compPeriods)
+        {
+            // Calculate the nominal interest rate
+            double nominal = NOMINAL(rate / 100, compPeriods);
+
+            // Re-calculate nominal rate with regard to the compounding periods
+            var compRate = nominal / compPeriods;
+
+            // Calculate first part
+            double firstPart = Math.Abs(presentValue) * Math.Pow(1 + compRate, periods);
+
+            // Get the periodic payments
+            var result = (futureValue - firstPart) / ((Math.Pow((1 + compRate), periods) - 1) / compRate);
+
+            // Format the output
+            return -1 * Math.Round(Math.Abs(result), 4);
+        }
+
+        /// <summary>
+        /// Interest rate
+        /// </summary>
+        /// <param name="presentValue">Present value (initial capital)</param>
+        /// <param name="futureValue">Future value of the investment</param>
+        /// <param name="periods">Total number of periods</param>
+        /// <param name="compPeriods">Number of compounding periods per year</param>
+        /// <param name="payment">Amount of the constant periodic payment</param>
+        public static double INTRATE(double presentValue, double futureValue, int periods, int compPeriods, double payment)
+        {
+            var seeker = new InterestRateSeeker(presentValue, periods, compPeriods, payment);
+            var goalSeeker = new TridentGoalSeek.GoalSeek(seeker);
+            var result = goalSeeker.SeekResult(Convert.ToDecimal(futureValue));
+            return Math.Round(Convert.ToDouble(result.InputVariable), 2);
+        }
+
+        /// <summary>
+        /// Used to find the Interest Rate using the Goal Seek algorythm
+        /// </summary>
+        class InterestRateSeeker : TridentGoalSeek.IGoalSeekAlgorithm
+        {
+            public double PresentValue { get; set; }
+            public int Periods { get; set; }
+            public int CompPeriods { get; set; }
+            public double Payment { get; set; }
+
+            public InterestRateSeeker(double presentValue, int periods, int compPeriods, double payment)
+            {
+                PresentValue = presentValue;
+                Periods = periods;
+                CompPeriods = compPeriods;
+                Payment = payment;
+            }
+
+            public decimal Calculate(decimal rate)
+            {
+                return Convert.ToDecimal(FV(PresentValue, Convert.ToDouble(rate), Periods, CompPeriods, Payment));
+            }
+        }
+    }
+}
---
+++ Source/PersonalFinanceDemo/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Threading;
+
+namespace PersonalFinanceDemo
+{
+    class Program
+    {
+
+        private const string _welcomeString = "Welcome to Personal Financial Planner!\n";
+        private const string _optionsString =
+            "Please, select one of the options below:\n" +
+            "0: Exit the program.\n" +
+            "1: Calculate the effective interest rate of the investment.\n" +
+            "2: Calculate the future value of the investment.\n" +
+            "3: Calculate the constant periodic payment.\n" +
+            "4: Calculate the initial capital required for the investment.\n";
+
+        private const string _optionsNextString = "Please, select one of the options (0-4): ";
+
+        // Command codes
+        private const string _exitCode = "0";
+        private const string _calculateInterestRate = "1";
+        private const string _calculateFutureValue = "2";
+        private const string _calculatePayment = "3";
+        private const string _calculatePresentValue = "4";
+
+        private const string _interestRateIs = "Required interest rate is: ";
+        private const string _presentValueIs = "Required present value is: ";
+        private const string _futureValueWillBe = "Future value will be: ";
+        private const string _paymentsWillBe = "Required periodic payments will be: ";
+
+        private const string _enterPresentValue = "Enter the present value of the invesment: ";
+        private const string _enterFutureValue = "Enter the future value of the investment: ";
+        private const string _enterPeriods = "Enter the total number of periods: ";
+        private const string _enterCompPeriods = "Enter the number of compounding periods per year: ";
+        private const string _enterPayment = "Enter the amount of periodic payments: ";
+        private const string _enterEffectiveRate = "Enter the annual(!) effective interest rate (%): ";
+
+        private const int _defaultPresentValue = -20000;
+        private const int _defaultCompundingPeriods = 12;
+        private const int _defaultTotalPeriods = 120;
+        private const int _defaultPayment = -100;
+
+        static void Main(string[] args)
+        {
+            // Get the welcome message
+            string welcome = _welcomeString + _optionsString;
+
+            // Show the welcome message to user
+            Console.Write(welcome);
+
+            // User action code
+            string code = string.Empty;
+
+            // Wait for the code from user
+            while (code != _exitCode)
+            {
+                // Read the user input
+                code = Console.ReadLine();
+
+                try
+                {
+                    // Handle the code
+                    switch (code)
+                    {
+                        case _calculateInterestRate:
+                            {
+                                double presentValue = AskDouble(_enterPresentValue, _defaultPresentValue);
+                                double futureValue = AskDouble(_enterFutureValue);
+                                int periods = AskInteger(_enterPeriods, _defaultTotalPeriods);
+                                int compPeriods = AskInteger(_enterCompPeriods, _defaultCompundingPeriods);
+                                double payment = AskDouble(_enterPayment, _defaultPayment);
+
+                                double rate = Functions.INTRATE(presentValue, futureValue, periods, compPeriods, payment);
+                                Console.WriteLine(_interestRateIs + rate.ToString() + "%");
+
+                                break;
+                            }
+                        case _calculateFutureValue:
+                            {
+                                double presentValue = AskDouble(_enterPresentValue, _defaultPresentValue);
+                                double rate = AskDouble(_enterEffectiveRate);
+                                int periods = AskInteger(_enterPeriods, _defaultTotalPeriods);
+                                int compPeriods = AskInteger(_enterCompPeriods, _defaultCompundingPeriods);
+                                double payment = AskDouble(_enterPayment, _defaultPayment);
+
+                                double futureValue = Functions.FV(presentValue, rate, periods, compPeriods, payment);
+                                Console.WriteLine(_futureValueWillBe + futureValue.ToString());
+
+                                break;
+                            }
+                        case _calculatePayment:
+                            {
+                                double presentValue = AskDouble(_enterPresentValue, _defaultPresentValue);
+                                double futureValue = AskDouble(_enterFutureValue);
+                                double rate = AskDouble(_enterEffectiveRate);
+                                int periods = AskInteger(_enterPeriods, _defaultTotalPeriods);
+                                int compPeriods = AskInteger(_enterCompPeriods, _defaultCompundingPeriods);
+
+                                double payment = Math.Round(Functions.PMT(presentValue, futureValue, rate, periods, compPeriods), 4);
+                                Console.WriteLine(_paymentsWillBe + payment.ToString());
+
+                                break;
+                            }
+                        case _calculatePresentValue:
+                            {
+                                double futureValue = AskDouble(_enterFutureValue);
+                                double rate = AskDouble(_enterEffectiveRate);
+                                int periods = AskInteger(_enterPeriods, _defaultTotalPeriods);
+                                int compPeriods = AskInteger(_enterCompPeriods, _defaultCompundingPeriods);
+                                double payment = AskDouble(_enterPayment, _defaultPayment);
+
+                                double presentValue = Functions.PV(futureValue, rate, periods, compPeriods, payment);
+                                Console.WriteLine(_presentValueIs + presentValue.ToString());
+
+                                break;
+                            }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Display the exception text
+                    Console.WriteLine(ex.Message);
+                }
+
+                // Line-break for better readability
+                Console.WriteLine();
+
+                // Display the options again
+                Console.Write(_optionsNextString);
+            }
+        }
+
+        private static double AskDouble(string message, double defaultValue = 0)
+        {
+            Console.Write(message);
+            string separator = Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            string sendKey = (defaultValue == 0) ? "0" + separator + "0" : defaultValue.ToString();
+            System.Windows.Forms.SendKeys.SendWait(sendKey);
+            return double.Parse(Console.ReadLine());            
+        }
+
+        private static int AskInteger(string message, int defaultValue)
+        {
+            Console.Write(message);
+            System.Windows.Forms.SendKeys.SendWait(defaultValue.ToString());
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
